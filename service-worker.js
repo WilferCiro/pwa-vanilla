0a3ba168009484f0b24a2efe96e5c5d9fee903d2
Wilfer Daniel Ciro Maya <ciro@DESKTOP-RLBAN4Q>
2023-11-20 21:18:20 -0500

    feat: add icons to notifications

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
-const CACHE_NAME = 'my-cache-v2';
+const CACHE_NAME = "my-cache-v2";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/effect.css',
-  '/js/main.js',
-  '/js/effect.js',
-  '/service-worker.js',
-  '/images/android-chrome-192x192.png',
+  "/",
+  "/index.html",
+  "/css/effect.css",
+  "/js/main.js",
+  "/js/effect.js",
+  "/service-worker.js",
+  "/images/android-chrome-192x192.png",
 ];
 
 self.addEventListener("install", function (event) {
-  console.log('Service Worker instalado');
+  console.log("Service Worker instalado");
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
-  // self.skipWaiting();  
+  // self.skipWaiting();
 });
 
 self.addEventListener("activate", function (event) {
@@ -24,16 +23,19 @@ self.addEventListener("activate", function (event) {
   }
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
 
 self.addEventListener("push", function (event) {
   const options = {
     body: event.data.text(),
+    icon: "/images/android-chrome-512x512.png",
+    badge: "/images/demos/android-chrome-192x192.png",
   };
 
   event.waitUntil(

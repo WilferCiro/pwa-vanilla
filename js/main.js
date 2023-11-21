b89432cbc2e4e01f32b431b2cf5abbbed77063d2
Wilfer Daniel Ciro Maya <ciro@DESKTOP-RLBAN4Q>
2023-11-20 20:45:37 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+if ("serviceWorker" in navigator && "PushManager" in window) {
+  navigator.serviceWorker
+    .register("./js/service-worker.js")
+    .then(function (registration) {
+      console.log("Service Worker registrado con éxito:", registration);
+    })
+    .catch(function (error) {
+      console.error("Error al registrar el Service Worker:", error);
+    });
+}
+
+Notification.requestPermission().then(function (permission) {
+  if (permission === "granted") {
+    subscribeToPushNotifications();
+  }
+});
+
+function sendNotification(title, body) {
+  navigator.serviceWorker.ready.then(function (registration) {
+    registration.showNotification(title, {
+      body: body,
+      icon: "../images/android-chrome-192x192.png",
+    });
+  });
+}
+
+function subscribeToPushNotifications() {
+  navigator.serviceWorker.ready.then(function (registration) {
+    registration.pushManager
+      .subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          "BIWuK6InmsQ47o25F37l6ejXFUgKeQUa0DeXHVJWmhAacNftN4juHMJI9vmJbjKaE9G81OJaMi6PFFxzv7mY05s"
+        ),
+      })
+      .then(function (subscription) {
+        fetch("https://socket-io-4ed0.onrender.com/subscribe", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(subscription),
+        });
+      })
+      .catch(function (error) {
+        console.error("Error al suscribirse a notificaciones push:", error);
+      });
+  });
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+
+  return outputArray;
+}
---
+++ js/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'my-cache-v2';
+const urlsToCache = [
+  '/',
+  '../index.html',
+  '../css/effect.css',
+  '../js/main.js',
+  '../js/effect.js',
+  '../js/service-worker.js',
+  '../images/android-chrome-192x192.png',
+];
+
+self.addEventListener("install", function (event) {
+  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  // self.skipWaiting();  
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("ACTIVATED");
+  if (self.clients && clients.claim) {
+    clients.claim();
+  }
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Solicitud fetch:', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("push", function (event) {
+  const options = {
+    body: event.data.text(),
+  };
+
+  event.waitUntil(
+    self.registration.showNotification("Título de la Notificación", options)
+  );
+});
